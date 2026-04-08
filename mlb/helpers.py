3ce38e95ec8c7b313ce2dfca31df4a1ad171dea0
bettormanpicks <bettormanpicks@gmail.com>
2026-04-07 21:00:55 -0500

    updated mlb

--- mlb/helpers.py
+++ mlb/helpers.py
@@ -11,8 +11,9 @@ def load_mlb_raw_data():
     box = pd.read_csv("mlb/data/mlb_cleaned_boxscores.csv")
     schedule = pd.read_csv("mlb/data/2026_mlb_schedule.csv")
 
-    box["date"] = pd.to_datetime(box["date"]).dt.tz_localize("US/Eastern")
-    schedule["date"] = pd.to_datetime(schedule["date"]).dt.tz_localize("US/Eastern")
+    eastern = pytz.timezone("US/Eastern")
+    box["date"] = pd.to_datetime(box["date"]).dt.tz_localize(eastern)
+    schedule["date"] = pd.to_datetime(schedule["date"]).dt.tz_localize(eastern)
 
     return box, schedule
 
