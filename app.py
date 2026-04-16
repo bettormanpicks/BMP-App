9d6d57b1c8a1a81dc03f157f9280541dce767eb0
bettormanpicks <bettormanpicks@gmail.com>
2026-04-15 20:25:11 -0500

    updated app

--- app.py
+++ app.py
@@ -1391,6 +1391,7 @@ with streamlit_analytics.track():
             st.dataframe(
                 df_display[display_cols].sort_values("Match Start"),
                 width="stretch",
+                height=650,
                 hide_index=True,
                 column_config=col_config
             )
