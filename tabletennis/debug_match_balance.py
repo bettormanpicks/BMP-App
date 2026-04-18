3722ba10c4cfe11943198bf32e340140eaf4ab10
bettormanpicks <bettormanpicks@gmail.com>
2026-04-18 03:30:22 -0500

    Automated NBA logs update: 2026-04-18 03:30

--- tabletennis/debug_match_balance.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/tt_czech_features_weighted.csv")
-
-print(df["match_balance_L10"].describe())
--- tabletennis/debug_match_balance_test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/tt_czech_features_weighted.csv")
-
-print((df["match_balance_L10"] == 0).sum())
-print(df.sort_values("match_balance_L10").head(10))
-print()
-print(df.sort_values("match_balance_L10").tail(10))
